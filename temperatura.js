fbc938d811168871647a169951164e88f5f38e86
Romilson Monteiro <romilson.tricolor@gmail.com>
2022-05-25 21:53:50 -0300

    Inclusão do conversor de temperatura

---
+++ temperatura.js
@@ -0,0 +1,27 @@
+function ConverterTemperatura(){
+
+    if(document.getElementById("Centigrados").value == "" && document.getElementById("Fahrenheit").value == ""){
+        alert("O preenchimento de uma temperatura é obrigatório!")
+        // document.getElementById("Centigrados").focus();
+        return;
+    }
+
+    if(document.getElementById("Fahrenheit").value == ""){
+    let c = document.getElementById("Centigrados").value;
+    let f = (9 * c + 160) / 5;
+    document.getElementById("Fahrenheit").value = f;
+
+    if(f > 50){
+        document.getElementById("Fahrenheit").style.backgroundColor = "red";
+    }else {
+        document.getElementById("Fahrenheit").style.backgroundColor = "blue";
+    }
+
+    }else {
+    let f = document.getElementById("Fahrenheit").value;
+    let c = (f - 32) * 5  / 9;
+    document.getElementById("Centigrados").value = c;
+
+    }
+
+}
